6c823c52691bfc2dc1de4c58a0638fdf676afc68
ibrahim-muchiri <ibrahimainge@gmail.com>
2020-08-20 19:01:03 +0300

    first commit

---
+++ server1.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+
+
+app.get('/', function(req, res)
+{
+    res.send('<h1>express works!</h1>');
+});
+
+app.listen(3000, function ()
+{
+    
+    console.log('server listening to port 3000');
+});
